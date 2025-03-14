1abd57d66179a2f06fe8f918bf27602b6e135029
Andrew Kungu <drewskungu@gmail.com>
2025-03-14 22:36:18 +0300

    My If functions

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet( somevalue ){
+
+  let result 
+ if (somevalue <= 400){result = "This one is on me!" ; 
+  return result
+ }
+ else if (somevalue <=2000){result = "That will be twenty bucks." ; 
+ return result}
+ else if (somevalue < 2500){result= "I will gladly take your thirty bucks." ;
+  return result
+ }
+ else {result= "No can do.";
+  return result
+ }
+
+  } 
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(1500)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
+
+
+
+
+  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  let result
+  city==="NYC" ? (result="Ok, sounds good.") : ( result="No go.")
+return result
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let result 
+  switch (tip) {
+    case 'generous':
+      result = "Thank you so much.";
+      return result;
+      break;
+     
+    case "not as generous":
+      result = "Thank you.";
+      return result;
+      break;
+    default: 
+      result = "Bye.";
+      return result
+      break;
+      
+      
+  }
 
-function switchOnCharmFromTip(){
   // Write your code here!
 }
+switchOnCharmFromTip(generous)
+switchOnCharmFromTip(not_as_generous)
+switchOnCharmFromTip (thanks_for_everything)
